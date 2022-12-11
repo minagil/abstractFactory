75d321cbc099851c807e3dfe3260f4cd7fbec115
mina <rlfalsk0301@gmail.com>
2022-12-11 15:36:49 +0900

    abstract class and method usege

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-// const coffeeFactory = (function(){
-//   const types = {};
-//   return {
-//     addType: function(type, Coffee){
-//       if(Coffee.prototype.order){
-//         types[type] = Coffee;
-//       }
-//     },
-//     create: function(type, options){
-//       const Coffee = types[type];
-//       return (Coffee ? new Coffee(options) : undefined);
-//     }
-//   }
-// })();
+const coffeeFactory = (function(){
+  const types = {};
+  return {
+    addType: function(type, Coffee){
+      if(Coffee.prototype.order){
+        types[type] = Coffee;
+      }
+    },
+    create: function(type, options){
+      const Coffee = types[type];
+      return (Coffee ? new Coffee(options) : undefined);
+    }
+  }
+})();
 
-// const Coffee = (function(){
-//   function Coffee(options){
-//     this.name = options.name;
-//     this.price = options.price;
-//   }
-//   Coffee.prototype.order = function(){
-//     console.log(`${this.name}를 ${this.price}에 주문합니다.`);
-//   }
-//   Coffee.prototype.orderCancel = function(reason){
-//     console.log(`${this.name}를 ${reason}의 사유로 주문을 취소합니다. ${this.price}를 환불합니다.`);
-//   }
-//   return Coffee
-// })();
+const Coffee = (function(){
+  function Coffee(options){
+    this.name = options.name;
+    this.price = options.price;
+  }
+  Coffee.prototype.order = function(){
+    console.log(`${this.name}를 ${this.price}에 주문합니다.`);
+  }
+  Coffee.prototype.orderCancel = function(reason){
+    console.log(`${this.name}를 ${reason}의 사유로 주문을 취소합니다. ${this.price}를 환불합니다.`);
+  }
+  return Coffee
+})();
 
-// coffeeFactory.addType('Americano', Coffee);
-// const IceAmericano = coffeeFactory.create('Americano', {name: 'Ice Americano', price: 3500});
+coffeeFactory.addType('Americano', Coffee);
+const IceAmericano = coffeeFactory.create('Americano', {name: 'Ice Americano', price: 3500});
 
-// IceAmericano.order();
-// IceAmericano.orderCancel('변심');
+IceAmericano.order();
+IceAmericano.orderCancel('변심');
+
+/*********************************************************************/
 
 function Car(options){
   this.doors = options.doors || 4;
@@ -93,41 +95,76 @@ var myBigTruck = truckFactory.createVehicle({
 console.log(myBigTruck instanceof Truck);
 console.log(myBigTruck)
 
-// function Shape(){
-//   this.name = 'Shape';
-//   this.getName = function(){
-//     return this.name;
-//   }
-// }
+function Shape(){
+  this.name = 'Shape';
+  this.getName = function(){
+    return this.name;
+  }
+}
 
-// function TwoDShape(){
-//   this.name = '2DShape';
-// }
+function TwoDShape(){
+  this.name = '2DShape';
+}
 
-// function Triangle(side, height){
-//   this.name = 'Triangle';
-//   this.side = side;
-//   this.height = height;
-//   this.getArea = function(){
-//     return (this.side * this.height) / 2;
-//   }
-// }
+function Triangle(side, height){
+  this.name = 'Triangle';
+  this.side = side;
+  this.height = height;
+  this.getArea = function(){
+    return (this.side * this.height) / 2;
+  }
+}
 
-// //상속을 구현하는 코드
-// TwoDShape.prototype = new Shape();
-// TwoDShape.prototype.constructor = TwoDShape;
-// Triangle.prototype = new TwoDShape();
-// Triangle.prototype.constructor = Triangle
+//상속을 구현하는 코드
+TwoDShape.prototype = new Shape();
+TwoDShape.prototype.constructor = TwoDShape;
+Triangle.prototype = new TwoDShape();
+Triangle.prototype.constructor = Triangle
 
-// const s = new Shape();
-// const td = new TwoDShape();
-// const t = new Triangle(10, 10);
-// console.log(t.getName())
-// console.log(s.constructor);
-// console.log(td.constructor);
-// console.log(t.constructor);
+const s = new Shape();
+const td = new TwoDShape();
+const t = new Triangle(10, 10);
+console.log(t.getName())
+console.log(s.constructor);
+console.log(td.constructor);
+console.log(t.constructor);
 
-// console.log(t instanceof Triangle);
-// console.log(t instanceof Triangle);
-// console.log(t instanceof Triangle);
+console.log(t instanceof Triangle);
+console.log(t instanceof Triangle);
+console.log(t instanceof Triangle);
 
+
+/*********************************************************************/
+
+var Animal = function(){
+  this.name = "";
+  if(this.constructor === Animal) {
+    throw new Error('정의된 추상클래스는 자기 자신의 객체를 가질 수 없습니다.');
+  }else{
+    return this;
+  }
+  
+} 
+Animal.prototype.say = function(){
+  throw new Error("Abstract method!")
+}
+Animal.prototype.getName = function(){
+  return this.name;
+}
+Animal.prototype.view = function(){}
+
+var Cat = function(){
+  Animal.apply(this, arguments);
+  this.name = "고양이";
+}
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+Cat.prototype.say = function(){
+  console.log("meow");
+}
+Cat.prototype.view = function(){
+  console.log('동물의 이름은 ' + this.name + '입니다.');
+}
+var cat = new Cat();
+cat.say();
+cat.view();
